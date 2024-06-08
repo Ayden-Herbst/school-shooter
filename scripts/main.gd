178e6249f8b94ba9c2fbf6c4a602f8d28aacc422
Ayden Herbst <ayden.herbst@students.pss.com.na>
2024-06-08 23:21:39 +0100

    Spring Cleaning

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,33 +1,20 @@
 extends Node2D
 
-var player = null
 var en = null
-# creating a variable that will receive its value on runtime (@onready)
-# accesses and stores the spwan point node
-@onready var player_spawn_point = $PlayerSpawnPoint
-@onready var laser_container = $LaserContainer
 
-# this is worth explaining to make it easier to load the enemies into the scene were creating an array, saw them in oddly enough godot has a data type called packed scene
-# which essentially stores scenes. Later on, we're going to randomly render one of the items in the Surrey
-
+@export var player_spawn_point: Node
+@export var laser_container: Node
 @export var enemy_scenes: Array[PackedScene] = []
 
-# accessing the timer which dictates when an enemy gets spawned and the container in which we spawn enemies
-@onready var timer = $EnemySpawnTimer
-@onready var enemy_container = $EnemyContainer
+@export var timer: Node
+@export var enemy_container: Node
+@export var player: Node
 
-# Called when the node enters the scene tree for the first time.
 func _ready():
-	# Giving the game access to the player node 
-	# (the node is stored in a  'group' called player)
-	player = get_tree().get_first_node_in_group("player")
-	# throws an error if player not found
 	assert(player!=null)
 	player.global_position = player_spawn_point.global_position
-	# Just giving us access to that laser shot signal from earlier
 	player.laser_shot.connect(_on_player_laser_shot)
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(_delta):
 	if Input.is_action_just_pressed("quit"):
 		get_tree().quit()
@@ -35,15 +22,12 @@ func _process(_delta):
 		get_tree().reload_current_scene()
 
 func _on_player_laser_shot(laser_scene, location):
-	# creating a laser instance
 	var laser = laser_scene.instantiate()
 	laser.global_position = location
 	print(location)
 	laser_container.add_child(laser)
 
 func _on_enemy_spawn_timer_timeout():
-	# the most inefficient piece of code yet. As we don't want any equivalent amount of worksheets and project spawned, 
-	# I have a variable which is assigned to a number from one to seven and if that number one will spawn a project and if it's anything greater than one wall spawn, a worksheet or regular enemy.
 	print('woof')
 	var sting_op = randi_range(1, 7)
 	print(sting_op)
--- scripts/player.gd
+++ scripts/player.gd
@@ -1,54 +1,39 @@
 class_name Player extends CharacterBody2D
 
-#defining speed, health and starting ammumition
-@export var speed = 550.0
-@export var hp = 100
-@export var ammo = 5
+@export var speed:int = 550
+@export var hp:int = 100
+@export var ammo:int = 5
 
-# getting laser scene to be accessed
-var laser_scene = preload("res://scenes/laser.tscn")
-
-# so basically, we're just creating a signal here that will send the laser scene and the location where it should be spawned to the main.gd script
 signal laser_shot(laser_scene, location)
-
-# we're doing the same thing here for the health and the ammo, but were just sending their values to the HUD.gd script
 signal health_changed(new_hp)
 signal ammo_changed(new_ammo)
 
-# and here we're just acting the muzzle which is just the pointed which we want the bullets to spawn relative to the player
-@onready var muzzle = $Muzzle
+@export var muzzle: Node
+var laser_scene = preload("res://scenes/laser.tscn")
 
-# Again, this process function runs once every single frame, so we're checking if shoot was pressed, and if so, running the shoot function
 func _process(delta):
 	if Input.is_action_just_pressed("shoot"):
 		shoot()
 		
-
 func _physics_process(delta):
-	# Getting the absolute direction in which the player wishes to move
-	# Essentially creates a vector w/ 2 properties (x, x) to indicate direction
-	var direction = Vector2(		
+	var direction = Vector2(
 		Input.get_axis("move_left", "move_right"), # Horizontal Axis
 		Input.get_axis("move_up", "move_down") # Vertical Axis
 		)
-	
-	# velocity is already part of the move_and_slide() function. We are just assigning it
+		
 	velocity = direction * speed
 	move_and_slide()
-	
-# sends a signal telling the game where the make the bullet appear from
+
 func shoot():
-	# we're controlling and altering the players ammunition here
 	if ammo > 0:
 		ammo -= 1
 		laser_shot.emit(laser_scene, muzzle.global_position)
-		# this signal will then be received by the hud and display the ammo
 		emit_signal("ammo_changed", ammo)
 	else: 
 		print('out of ammo')
 	
-# handles our health reduction
-func reduce_health(amount):
+
+func reduce_health(amount:float):
 	hp -= amount
 	hp = max(hp, 0)  # Ensure hp doesn't go below 0
 	hp = min(hp, 100)
@@ -56,23 +41,19 @@ func reduce_health(amount):
 	if hp <= 0:
 		die()
 
-# handles our health regeneration, this could be consolidated into a single function, but I don't feel like doing that now
-func increase_health(amount):
+func increase_health(amount:float):
 	hp += amount
-	hp = max(hp, 0)  # Ensure hp doesn't go below 0
+	hp = max(hp, 0) 
 	hp = min(hp, 100)
 	emit_signal("health_changed", hp)
 	print(amount)
 
-# This will just handle the increase in ammunition whenever an enemy is killed
-func reload(amount):
+func reload(amount:int):
 	if ammo < 9:
 		ammo += amount
 		emit_signal("ammo_changed", ammo)
 	print(ammo)
 
-
 func die():
-	# Handle the player's death here
 	queue_free()
 	
